c1acc27ac6c21740301b233adbee19c20370486a
JimmySoli <jimmynb2@gmail.com>
2024-01-25 21:42:34 -0300

    segundo commit

--- main.dart
+++ main.dart
@@ -1,7 +1,33 @@
 void main () {
-  String nome = "";
-  int xp = 0;
+  String nome = "Razor";
+  int xp = 6500;
 
-  
-  print ("jimmy ");
+  switch (xp) {
+    case <= 1000:
+      print ("O heroi $nome, está na categoria Ferro.");
+      break;
+
+      case <= 2000:
+      print ("O heroi $nome, está na categoria Bronze.");
+
+      case <= 5000:
+      print ("O heroi $nome, está na categoria Prata.");
+
+      case <= 7000:
+      print ("O heroi $nome, está na categoria Ouro.");
+
+      case <= 8000:
+      print ("O heroi $nome, está na categoria Platina.");
+
+      case <= 9000:
+      print ("O heroi $nome, está na categoria Ascendente.");
+
+      case <= 10000:
+      print ("O heroi $nome, está na categoria Imortal.");
+       
+    default:
+      print ("O heroi $nome, está na categoria Radiante.");
+
+  }
+
 }
